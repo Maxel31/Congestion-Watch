ed0e050f48b6ed56e3e2bd20126692f8b52b0ba6
nharu-0630 <root@nharu.dev>
2025-07-06 22:55:27 +0900

    fix: envを読むように修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,30 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
-
-const API_BASE_URL = process.env.VITE_API_BASE_URL || 'http://backend:8080';
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const API_BASE_URL = env.API_BASE_URL || 'http://backend:8080';
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: API_BASE_URL,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '/api'),
+        }
       }
     }
-  }
-})
+  })
+};
