23a9b865fdad8f07e61d98dda2605f72c02a5494
Mizuki Baba <info@lovelovetrb.com>
2025-07-10 13:52:28 +0900

    fix: backendがviewを参照せずテーブル結合を行っていた問題を修正

--- backend/repository/cloud_data_repository.go
+++ backend/repository/cloud_data_repository.go
@@ -15,24 +15,13 @@ func NewCloudDataRepository(db *sql.DB) CloudDataRepository {
 
 func (r *cloudDataRepository) GetCloudDataByDate(placeID int, targetDate string) ([]model.CloudData, error) {
 	query := `
-		SELECT 
-			ps.score as predicted_score,
-			acs.score as actual_score,
-			ps.target_datetime,
-			ps.place_id
-		FROM (
-			SELECT DISTINCT ON (place_id, target_datetime)
-				place_id,
+		SELECT
+				predicted_score,
+				actual_score,
 				target_datetime,
-				score
-			FROM predicted_score
-			WHERE place_id = $1 AND DATE(target_datetime) = DATE($2)
-			ORDER BY place_id, target_datetime, created_at DESC
-		) ps
-		LEFT JOIN actual_score acs
-			ON ps.place_id = acs.place_id 
-			AND ps.target_datetime = acs.target_datetime
-		ORDER BY ps.target_datetime DESC, ps.place_id, ps.score DESC
+				place_id
+		FROM cloud_data
+		WHERE place_id = $1 AND target_datetime::date = $2;
 	`
 
 	rows, err := r.db.Query(query, placeID, targetDate)
