9fc7a72555e357ec977c95e2c30441bacffc7dbb
Mizuki Baba <info@lovelovetrb.com>
2025-07-10 13:49:32 +0900

    fix: DBのviewにおいてacturel_scoreとpredicted_scoreのどちらかがある時にカラムを生成するように変更

--- database/schema/07_cloud_data_view.sql
+++ database/schema/07_cloud_data_view.sql
@@ -6,6 +6,7 @@
     
 DROP VIEW IF EXISTS cloud_data;
 CREATE VIEW cloud_data AS
+-- predicted_scoreがあってactual_scoreもある場合
 SELECT 
   ps.score as predicted_score,
   acs.score as actual_score,
@@ -19,6 +20,35 @@ FROM (
   FROM predicted_score
   ORDER BY place_id, target_datetime, created_at DESC
 ) ps
-LEFT JOIN actual_score acs
+LEFT JOIN (
+  SELECT DISTINCT ON (place_id, target_datetime)
+    place_id,
+    target_datetime,
+    score
+  FROM actual_score
+  ORDER BY place_id, target_datetime, created_at DESC
+) acs
   ON ps.place_id = acs.place_id 
-  AND ps.target_datetime = acs.target_datetime;
+  AND ps.target_datetime = acs.target_datetime
+
+UNION
+
+-- actual_scoreがあってpredicted_scoreがない場合
+SELECT 
+  NULL as predicted_score,
+  acs.score as actual_score,
+  acs.target_datetime,
+  acs.place_id
+FROM (
+  SELECT DISTINCT ON (place_id, target_datetime)
+    place_id,
+    target_datetime,
+    score
+  FROM actual_score
+  ORDER BY place_id, target_datetime, created_at DESC
+) acs
+WHERE NOT EXISTS (
+  SELECT 1 FROM predicted_score ps 
+  WHERE ps.place_id = acs.place_id 
+  AND ps.target_datetime = acs.target_datetime
+);
